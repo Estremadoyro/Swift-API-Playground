49fc27fbf773cc82a09e96198be7cdaa1aef3127
Leonardo <lmestremadoyro@gmail.com>
2022-05-16 17:52:38 -0500

    functional programming

--- Practice.playground/Contents.swift
+++ Practice.playground/Contents.swift
@@ -496,6 +496,208 @@ struct SomeStruct {
 let randomStruct = SomeStruct()
 randomStruct.show()
 
+// MARK: - Functional Programming
+
+// - Immutable State
+// - No side effects
+var thing = 4
+func superHero() {
+  print("I'm supa hot fire")
+  thing = 5
+}
+
+print("original state: \(thing)")
+superHero()
+print("mutated state: \(thing)")
+enum RideCategory: String, CustomStringConvertible {
+  case family
+  case kids
+  case thrill
+  case scary
+  case relaxing
+  case water
+
+  var description: String {
+    return rawValue
+  }
+}
+
+typealias Minutes = Double
+struct Ride: CustomStringConvertible {
+  let name: String
+  let categories: Set<RideCategory>
+  let waitTime: Minutes
+
+  var description: String {
+    return "Ride –\"\(name)\", wait: \(waitTime) mins, " +
+      "categories: \(categories)\n"
+  }
+}
+
+// Dummy data
+let parkRides = [
+  Ride(name: "Raging Rapids",
+       categories: [.family, .thrill, .water],
+       waitTime: 45.0),
+  Ride(name: "Crazy Funhouse", categories: [.family], waitTime: 10.0),
+  Ride(name: "Spinning Tea Cups", categories: [.kids], waitTime: 15.0),
+  Ride(name: "Spooky Hollow", categories: [.scary], waitTime: 30.0),
+  Ride(name: "Thunder Coaster",
+       categories: [.family, .thrill],
+       waitTime: 60.0),
+  Ride(name: "Grand Carousel", categories: [.family, .kids], waitTime: 15.0),
+  Ride(name: "Bumper Boats", categories: [.family, .water], waitTime: 25.0),
+  Ride(name: "Mountain Railroad",
+       categories: [.family, .relaxing],
+       waitTime: 0.0)
+]
+
+/// # Alphabetical list of all ride names
+// Imperative Style
+func sortedNamesImp(of rides: [Ride]) -> [String] {
+  var sortedRides = rides
+  var key: Ride
+
+  for i in 0 ..< sortedRides.count {
+    key = sortedRides[i]
+    // From i to 0 (excluding) by -1
+    for j in stride(from: i, to: -1, by: -1) {
+      if key.name.localizedCompare(sortedRides[j].name) == .orderedAscending {
+        sortedRides.remove(at: j + 1)
+        sortedRides.insert(key, at: j)
+      }
+    }
+  }
+  var sortedNames: [String] = []
+  for ride in sortedRides {
+    sortedNames.append(ride.name)
+  }
+  return sortedNames
+}
+
+/// # Stride (Custom loop)
+for i in stride(from: 10, to: -1, by: -1) {
+  print("i: \(i)")
+}
+
+let sortedNames1 = sortedNamesImp(of: parkRides)
+
+func testSortedNames(_ names: [String]) {
+  let expected = ["Bumper Boats",
+                  "Crazy Funhouse",
+                  "Grand Carousel",
+                  "Mountain Railroad",
+                  "Raging Rapids",
+                  "Spinning Tea Cups",
+                  "Spooky Hollow",
+                  "Thunder Coaster"]
+  assert(names == expected)
+  print("✅ test sorted names = PASS\n-")
+}
+
+print(sortedNames1)
+testSortedNames(sortedNames1)
+
+print(sortedNames1)
+testSortedNames(sortedNames1)
+
+var originalNames: [String] = []
+for ride in parkRides {
+  originalNames.append(ride.name)
+}
+
+func testOriginalNameOrder(_ names: [String]) {
+  let expected = ["Raging Rapids",
+                  "Crazy Funhouse",
+                  "Spinning Tea Cups",
+                  "Spooky Hollow",
+                  "Thunder Coaster",
+                  "Grand Carousel",
+                  "Bumper Boats",
+                  "Mountain Railroad"]
+  assert(names == expected)
+  print("✅ test original name order = PASS\n-")
+}
+
+print(originalNames)
+testOriginalNameOrder(originalNames)
+
+/// # Filter
+let randomNumbers: [Int] = [1, 2, 4, 6, 7, 8]
+let evenNumbers: [Int] = randomNumbers.filter { $0 % 2 == 0 }
+print("even numbers: \(evenNumbers)")
+
+func waitTimeIsShort(_ ride: Ride) -> Bool {
+  return ride.waitTime < 15.0
+}
+
+let shortWaitTimeRides = parkRides.filter { waitTimeIsShort($0) }
+print("Rides with short wait time: \n\(shortWaitTimeRides)")
+
+/// # Map
+let rideNames = parkRides.map { $0.name }
+print(rideNames)
+testOriginalNameOrder(rideNames)
+
+/// # Sorted
+let sortedRideNames: [String] = rideNames.sorted(by: <)
+print("[Declarative] Sorted rideNames: \(sortedRideNames)")
+
+/// # Alphabetical list of all ride names
+func sortedNamesFP(_ rides: [Ride]) -> [String] {
+  let rideNames = parkRides.map { $0.name }
+  return rideNames.sorted(by: <)
+}
+
+let sortedNames2 = sortedNamesFP(parkRides)
+testSortedNames(sortedNames2)
+
+/// # Reduce
+// Parameter: Starting value of T
+// Parameter: Closure combines a value of T with other T element in the collction to produce another value
+// Closure:
+//        + Acumulator <T>
+//        + Next element
+let totalWaitTime = parkRides.reduce(0.0) { total, ride in
+  total + ride.waitTime
+}
+
+/// # Partial Functions
+func filter(for category: RideCategory) -> ((_: [Ride]) -> [Ride]) {
+  return { rides in
+    rides.filter { $0.categories.contains(category) }
+  }
+}
+
+let kidRideFilter = filter(for: .kids)
+print("Rides for kids: \(filter(for: .kids)(parkRides))")
+
+/// # Pure Functions
+func ridesWithWaitTimeUnder(_ waitTime: Minutes, from rides: [Ride]) -> [Ride] {
+  return rides.filter { $0.waitTime < waitTime }
+}
+
+let shortWaitRides = ridesWithWaitTimeUnder(15, from: parkRides)
+
+func testShortWaitRides(_ testFilter: (Minutes, [Ride]) -> [Ride]) {
+  let limit = Minutes(15)
+  let result = testFilter(limit, parkRides)
+  print("rides with wait less than 15 minutes:\n\(result)")
+  let names = result.map { $0.name }.sorted(by: <)
+  let expected = ["Crazy Funhouse",
+                  "Mountain Railroad"]
+  assert(names == expected)
+  print("✅ test rides with wait time under 15 = PASS\n-")
+}
+
+// Passing the body of the function to testFilter: (Minues, [Ride]) -> [Ride]
+testShortWaitRides(ridesWithWaitTimeUnder(_:from:))
+
+/// # Referential Transparency
+testShortWaitRides { waitTime, rides in
+  rides.filter { $0.waitTime < waitTime }
+}
+
 // MARK: - Atomic Properties (Property Wrapper)
 
 print("-- Atomic Properties --")
